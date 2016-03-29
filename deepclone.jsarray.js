208cd3721e80681dc59085f8e046f36f65c03bfa
Raja Sekar <rajasekar89be@gmail.com>
2016-03-29 16:58:37 +0530

    description updated

--- deepclone.jsarray.js
+++ deepclone.jsarray.js
@@ -4,7 +4,7 @@
  * var x = [1,2,3,4,5];
  * var y = x;
  * 
- * I general Array is pass by reference. If you assign array variable x to y, it will not be deep cloned.
+ * In general Array is pass by reference. If you assign array variable x to y, it will not be deep cloned.
  * It is just shallow cloned, meaning if you change the value of  x, it will change y value also and vice versa.
  * So this below one liner code will helps to deep clone the array.
  *
